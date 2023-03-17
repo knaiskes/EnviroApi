94f7db92d64c48844eb8b48776c7d8665656ca84
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-03-17 23:23:22 +0200

    Add src directory and basic structure for routes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import temperatureRoutes from './routes/temperature.js';
+import humidityRoutes from './routes/humidity.js';
+
+const app = express();
+
+app.use('/temperature', temperatureRoutes);
+app.use('/humidity', humidityRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
