3fb510a1a0067dd3a5100f87913acc71c09ca286
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-03-18 02:43:30 +0200

    Fetch temperature data from postgres

--- src/controllers/temperatureController.js
+++ src/controllers/temperatureController.js
@@ -1,14 +1,12 @@
-let dummyData = [
-    {
-	"id": 1,
-	"temperature": 20.20
-    },
-    {
-	"id": 2,
-	"temperature": 30
-    }
-];
+import createPool from '../config/db.js';
 
-export const getTemperature = (req, res) => {
-    res.status(200).json(dummyData);
-};
+const pool = createPool();
+
+export const getTemperature = async (req, res) => {
+    try {
+	const result = await pool.query('SELECT temperature FROM dht22');
+	res.status(200).json(result.rows);
+    } catch (err) {
+	console.error('There has been an error: ', err.stack);
+    }
+}
